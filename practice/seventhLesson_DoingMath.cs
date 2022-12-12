fe881d9176b7609cc517cea9b0bd309d65c75c77
Raquel Abarca <raquel@Raquels-MacBook-Pro.local>
2022-12-12 13:28:54 -0600

    seventh lesson

---
+++ practice/seventhLesson_DoingMath.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Program
+{
+public static void Main(string[] args)
+    {
+        int salary = 90000;
+        int tax = 15000;
+
+        int netSalary = salary-tax;
+        Console.WriteLine(netSalary);
+    }
+}

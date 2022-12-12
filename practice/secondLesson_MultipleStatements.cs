c409064cc0ab40acdeea0c5596a2673359d5f617
Raquel Abarca <raquel@Raquels-MacBook-Pro.local>
2022-12-12 12:40:55 -0600

    second lesson lesson

---
+++ practice/secondLesson_MultipleStatements.cs
@@ -0,0 +1,13 @@
+// A line of code is called a statement
+
+// C# is a case-sensitive language. This means that you need to pay attention to the correct input of uppercase and lowercase letters.
+
+using System;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("My first C# Code Coach!");
+        Console.WriteLine("This is incredible!");
+    }
+}

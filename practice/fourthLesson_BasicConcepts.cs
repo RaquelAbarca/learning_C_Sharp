9ebdb67d08a851271256d02caff61df1ca40f8cf
Raquel Abarca <raquel@Raquels-MacBook-Pro.local>
2022-12-12 12:58:55 -0600

    fourth lesson

---
+++ practice/fourthLesson_BasicConcepts.cs
@@ -0,0 +1,10 @@
+Console.WriteLine("Hey!");
+Console.WriteLine("Happy to see you here!");
+
+Console.Write("Hey!");  // The Console.Write method doesn't set a new line for the next output.
+Console.Write("Happy to see you here!");
+
+/*
+This
+is a Multiline
+comment */

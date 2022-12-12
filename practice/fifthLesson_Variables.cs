fd7dc8f49fd81236afb64ea7f18bef3eb8127aea
Raquel Abarca <raquel@Raquels-MacBook-Pro.local>
2022-12-12 13:08:03 -0600

    fifth lesson

---
+++ practice/fifthLesson_Variables.cs
@@ -0,0 +1,19 @@
+string name = "Raquel"; // Initialization is when you assing a value to the variable
+
+name = "Sofia";
+
+Console.WriteLine(name);
+
+/*
+Rules of Naming Variables
+Let's talk about names.
+
+There are several naming rules that you need to follow in C#:
+
+variable names must begin with a letter or an underscore
+variable names cannot contain spaces
+variable names can only contain letters, numbers, or underscores
+C# is case-sensitive, which means that, for example, Name and name variables are different
+C# keywords can't be used as variable names. A keyword is a word that is part of the C# language
+💡It's very important to use variable names that are descriptive, which makes the code more understandable.
+*/

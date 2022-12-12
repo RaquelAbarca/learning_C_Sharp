e010efe37cd33ee7e1e611233af91bab77b9b7b2
Raquel Abarca <raquel@Raquels-MacBook-Pro.local>
2022-12-12 13:18:02 -0600

    sixth lesson

---
+++ practice/sixthLesson_DataTypes.cs
@@ -0,0 +1,15 @@
+/*
+string: Everything in double quotes is a string, even numbers.
+
+char: a single character. The character must be enclosed in single quotes ''.
+
+int: whole numbers
+
+double: decimal numbers
+
+float: uses less storage in memory, but is not as precise as the double type (postfix f)
+    [we have to use the postfix or it will me declare a double]
+decimal: is more precise than the double, and uses more memory (post fix m).
+
+bool: true/false
+*/
